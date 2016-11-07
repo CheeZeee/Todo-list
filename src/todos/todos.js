166a87909c00a4ce8004a8f179786c12c81033f8
Chika Chikwendu <Chika Chikwendu>
2016-11-07 14:27:53 +0100

    Add todo controller and buttons

---
+++ src/todos/todos.js
@@ -0,0 +1,12 @@
+export default function($scope) {
+  $scope.todos = [
+    {
+      task: 'do the dishes',
+      isCompleted: false
+    },
+    {
+      task: 'watch football',
+      isCompleted: true
+    }
+  ];
+}
