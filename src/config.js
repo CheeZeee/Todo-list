99774b76198721e54130ed58dabfb846136b1b1f
Chika Chikwendu <Chika Chikwendu>
2016-11-07 12:57:59 +0100

    Initial commit, add sass and bootstrap

---
+++ src/config.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+const app = angular.module('app', [uiRouter]);
+
+app.config(($stateProvider, $urlRouterProvider, $locationProvider) => {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('todos', {
+      url: '/',
+      template: require('todos/todos.html')
+    })
+    .state('about', {
+      url: '/about',
+      template: require('about/about.html')
+    })
+  $locationProvider.html5Mode(true); //this takes away the hash key in the URL each time we refresh the page
+});
+
+export default app;
