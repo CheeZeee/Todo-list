720fa26521b2e1831a07580b7c4572be117ed506
Chika Chikwendu <Chika Chikwendu>
2016-11-09 04:07:11 +0100

    Activate the addTask button

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,4 +1,8 @@
 export default function($scope) {
+  let params = {
+    taskInputField: false
+  };
+
   $scope.todos = [
     {
       task: 'do the dishes',
@@ -9,4 +13,24 @@ export default function($scope) {
       isCompleted: true
     }
   ];
+
+  $scope.completedTaskClick = todos => {
+    todos.isCompleted = !todos.isCompleted;
+  };
+
+  $scope.createTask = () => {
+    params.taskInputField = false;
+    $scope.taskInputField = '';
+  };
+
+  $scope.$watch('taskInputField', val => {
+    if (!val && params.taskInputField) {
+      $scope.todos.pop();
+    } else if (val && !params.taskInputField) {
+      $scope.todos.push({ task: val, isCompleted: false });
+      params.taskInputField = true;
+    } else if (val && params.taskInputField){
+      $scope.todos[$scope.todos.length - 1].task = val;
+    }
+  });
 }
